514c83fa4fa26a571da5a6a4f63ba9b67f00482d
PavelBandal <pavel.bandal@ya.ru>
2017-10-16 21:58:10 +0400

    123456677546

---
+++ domashka/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите возраст");
+            string ag = Console.ReadLine();
+            double age = int.Parse(ag);
+            if (age >= 10 && age < 20)
+            {
+                Console.WriteLine("Ты подросток");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Ты не подросток");
+            } Console.ReadLine();
+        }
+    }
+}
---
+++ domashka/ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите процент кислорода");
+            string o = Console.ReadLine();
+            int oxygen = int.Parse(o);
+            Console.WriteLine("Введите среднюю температуру");
+            string t = Console.ReadLine();
+            int temperature = int.Parse(t);
+            if (oxygen > 20 && temperature > 15)
+            {
+                Console.WriteLine("Хьюстоон!!!На планете есть жизнь,    но знакомство с ней обернулось потерей руки путём откусывания выше указанной жизнью!!!");
+                Console.ReadLine();
+            }   
+            else
+            {
+                Console.WriteLine("Хьюстон, планета необитаема.");
+            }   Console.ReadLine();
+        }
+    }
+}
