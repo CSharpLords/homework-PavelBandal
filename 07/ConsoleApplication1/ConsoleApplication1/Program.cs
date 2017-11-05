aa603e0647b7fd1871900500d4658f8372ba3061
PavelBandal <32432734+PavelBandal@users.noreply.github.com>
2017-11-05 14:57:21 +0400

    HelloDanger

---
+++ 07/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowMessage();
+        }
+        static void ShowMessage()
+        {
+            Console.WriteLine("Введите значение угрозы");
+            int znachenie = int.Parse(Console.ReadLine());
+            if (znachenie > 5)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Внимание!Внимание!");
+            }
+            else
+            {
+                Console.WriteLine("Привет!");
+            }
+        }
+    }
+}
