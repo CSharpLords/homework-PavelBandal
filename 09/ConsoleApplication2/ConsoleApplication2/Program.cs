2bd11c339391ce7765e932df75c4d1c6d02b7310
PavelBandal <32432734+PavelBandal@users.noreply.github.com>
2017-11-18 21:01:29 +0400

    цуйук2е4

---
+++ 09/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите первое число");
+                int a =int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите второе число");
+                int b = int.Parse(Console.ReadLine());
+                int c = a + b;
+                Console.WriteLine("Сумма чисел равна "+ c);
+                
+
+            }
+        }
+    }
+}
--- 09/MemoryTrainer/Program.cs
+++ 09/MemoryTrainer/Program.cs
@@ -15,25 +15,32 @@ namespace MemoryTrainer {
 			Random rand = new Random();
 
 			while (true) {
-				int number = rand.Next(100);
-
-				Console.WriteLine("Запомните число: " + number);
-				Thread.Sleep(2000);
-				Console.Clear();
-
-				Console.WriteLine("Введите запомненное число");
-				int guess = int.Parse(Console.ReadLine());
-				Console.WriteLine();
-
-				if (guess == number) {
-					Console.WriteLine("Вы угадали!");
-				}
-				else {
-					Console.WriteLine("Вы ошиблись ='(");
-				}
-				Thread.Sleep(1000);
-				Console.Clear();
+                int number = rand.Next(100);
+				
 			}
 		}
-	}
-}
+        static void VsaProga() {
+	    
+        
+
+		    Console.WriteLine("Запомните число: " + number);
+		    Thread.Sleep(2000);
+		    Console.Clear();
+
+		    Console.WriteLine("Введите запомненное число");
+		    int guess = int.Parse(Console.ReadLine());
+		    Console.WriteLine();
+
+		    if (guess == number) {
+			    Console.WriteLine("Вы угадали!");
+                    
+		    }
+		    else {
+			    Console.WriteLine("Вы ошиблись ='(");
+		    }
+		    Thread.Sleep(1000);
+		    Console.Clear();
+          
+
+
+
