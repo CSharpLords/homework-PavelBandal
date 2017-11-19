3e7826f5aed6caffd4664ced02eeabd5feb2647d
PavelBandal <32432734+PavelBandal@users.noreply.github.com>
2017-11-19 17:03:33 +0300

    123434343434343434343434343434343434343434

--- 09/MemoryTrainer/Program.cs
+++ 09/MemoryTrainer/Program.cs
@@ -5,41 +5,52 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace MemoryTrainer {
-	class Program {
-		static void Main(string[] args) {
-			GuessNumber();
-		}
+namespace MemoryTrainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GuessNumber();
+        }
 
-		static void GuessNumber() {
-			Random rand = new Random();
+        static void GuessNumber()
+        {
+            Random rand = new Random();
 
-			while (true) {
+            while (true)
+            {
                 int number = rand.Next(100);
-				
-			}
-		}
-        static void VsaProga() {
-	    
-        
 
-		    Console.WriteLine("Запомните число: " + number);
-		    Thread.Sleep(2000);
-		    Console.Clear();
+            }
+        }
+        static void VsaProga()
+        {
 
-		    Console.WriteLine("Введите запомненное число");
-		    int guess = int.Parse(Console.ReadLine());
-		    Console.WriteLine();
 
-		    if (guess == number) {
-			    Console.WriteLine("Вы угадали!");
-                    
-		    }
-		    else {
-			    Console.WriteLine("Вы ошиблись ='(");
-		    }
-		    Thread.Sleep(1000);
-		    Console.Clear();
+
+            Console.WriteLine("Запомните число: " + number);
+            Thread.Sleep(2000);
+            Console.Clear();
+
+            Console.WriteLine("Введите запомненное число");
+            int guess = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            if (guess == number)
+            {
+                Console.WriteLine("Вы угадали!");
+
+            }
+            else
+            {
+                Console.WriteLine("Вы ошиблись ='(");
+            }
+            Thread.Sleep(1000);
+            Console.Clear();
+        }
+    }
+}
           
 
 
