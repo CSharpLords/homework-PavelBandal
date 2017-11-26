98717f3063fffb53976f768905ab564ccc19dfa0
PavelBandal <32432734+PavelBandal@users.noreply.github.com>
2017-11-26 16:58:59 +0300

    нло

---
+++ 10/a/a/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Beep();
+            Console.WriteLine("Введите грузоподъёмнность корабля.");
+            int maxmassa = int.Parse(Console.ReadLine());
+            int power = 0;
+            {
+                while (true)
+                {
+                    if (power<maxmassa)
+                    {
+                        Console.WriteLine("Введите вес коровы.(Если корова жирнее слона не берите её!!!)");
+                        int massaa = int.Parse(Console.ReadLine());
+                        power = power + massaa;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Улетайте");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+      
+       
+
+        
+
+
+        
