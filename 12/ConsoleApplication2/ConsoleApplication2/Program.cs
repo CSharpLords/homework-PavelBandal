2abf926174a9aad2da1f3c4958c0133e5fb2b5f0
PavelBandal <32432734+PavelBandal@users.noreply.github.com>
2017-12-08 21:44:26 +0400

    Слова наоборот

---
+++ 12/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любое слово в 9 букв.");
+            string str =Console.ReadLine();
+            char a = str[0];
+            char b = str[1];
+            char c = str[2];
+            char d = str[3];
+            char e = str[4];
+            char f = str[5];
+            char g = str[6];
+            char h = str[7];
+            char i = str[8];
+            Console.WriteLine(h);
+            Console.WriteLine(g);
+            Console.WriteLine(f);
+            Console.WriteLine(e);
+            Console.WriteLine(d);
+            Console.WriteLine(c);
+            Console.WriteLine(b);
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
