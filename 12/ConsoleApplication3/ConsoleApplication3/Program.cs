1fdb216bd46e1a75020e7b459ae5176c212184f8
PavelBandal <32432734+PavelBandal@users.noreply.github.com>
2018-01-14 13:28:57 +0400

    3

---
+++ 12/ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] massiv = { 1, 2, 34, 5, 6, 7, 7, 8, 9 };
+            int a = 1;
+            int b = a + 1;
+            for (int i = 0; i == 9; a = a + 1)
+            {
+                if (massi]
+            }
+        }
+    }
+}
+
