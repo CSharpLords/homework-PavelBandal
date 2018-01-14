817d9fabe3f0a682a88e7e08f143e7465df5e6d1
PavelBandal <32432734+PavelBandal@users.noreply.github.com>
2018-01-14 14:36:30 +0400

    Summary
    Description

---
+++ 15/6/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomRost{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] growth = new int[12];
+            for (int i = 0; i < growth.Length; i = i + 1)
+            {
+                int r = rand.Next(163, 190);
+                growth[i] = r;
+                Console.WriteLine(growth[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
