4607dd629e5541a8cf8fecb3de9bbbef36ad3d79
PavelBandal <32432734+PavelBandal@users.noreply.github.com>
2018-05-13 16:30:31 +0300

    Котэ Комбат

--- 31/CatFactory/Cat.cs
+++ 31/CatFactory/Cat.cs
@@ -1,8 +1,55 @@
-﻿namespace CatFactory {
+﻿using System;
+
+namespace CatFactory {
 	class Cat {
 		public string name;
 		public double age;
 		public double weight;
 		public double strenght;
-	}
+        int score = 0;
+        public Cat (string name, double age, double weight, double strenght)
+        {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.strenght = strenght;
+        }
+        public string Fight(Cat kotic)
+        {
+            score = 0;
+            kotic.score = 0;
+            if (age > kotic.age)
+            {
+                score++;
+            }
+            else
+            {
+                kotic.score++;
+            }
+            if (weight > kotic.weight)
+            {
+                score++;
+            }
+            else
+            {
+                kotic.score++;
+            }
+            if (strenght > kotic.strenght)
+            {
+                score++;
+            }
+            else
+            {
+                kotic.score++;
+            }
+            if (kotic.score > score)
+            {
+                return kotic.name;
+            }
+            else
+            {
+                return name;
+            }
+        }
+    }
 }
